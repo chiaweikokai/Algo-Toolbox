04984add8cfd0cb8cc523ac8c48d87aacf1e32e6
chiaweikokai <chiaweikokai@gmail.com>
2016-07-10 22:46:10 +0900

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -2,15 +2,6 @@
 import sys
 
 
-# def gcd(a, b):
-#     current_gcd = 1
-#     for d in range(2, min(a, b) + 1):
-#         if a % d == 0 and b % d == 0:
-#             if d > current_gcd:
-#                 current_gcd = d
-#     return current_gcd
-
-
 def gcd(a, b):
     if b == 0:
         return a
